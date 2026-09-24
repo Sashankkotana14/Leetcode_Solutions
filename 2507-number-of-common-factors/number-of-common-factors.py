75923bd548a6e06cfec812b174228545f81b014d
Subramanya Sai Sashank Kotana <sashankkotana@gmail.com>
2026-09-24 10:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (56.32%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        count=0
+        A=min(a,b)
+        for i in range(1,A+1):
+            
+            if a%i==0 and b%i==0:
+                count+=1
+
+        return count            
+
+        
+        
