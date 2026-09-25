4ddad54fcac9c3f54092412d3f9e0e372eeb1517
Subramanya Sai Sashank Kotana <sashankkotana@gmail.com>
2026-09-25 08:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (0.68%) - LeetSync

---
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        count=0
+        for i in range(0,len(hours)):
+            if hours[i]>=target:
+                count+=1
+        return count        
+        
+        
